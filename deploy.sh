fbacff6ba5280232cf5e5d8eb10791381fcb90dd
wrule <1982775886@qq.com>
2024-03-27 11:44:21 +0800

    chore: depy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # perfma@888!
-# ssh root@10.10.222.240 "cd /data/wwwroot/client && bash ./sync.sh"
+ssh root@10.10.222.240 "source ~/.nvm/nvm.sh && cd /data/wwwroot/client && bash ./sync.sh"
 # zZVhsiWS
 ssh root@10.10.222.154 "cd /data && bash ./xen-update.sh"
