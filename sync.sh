f47b5054181bb778443b87523871acb68c949bed
wrule <1982775886@qq.com>
2024-03-28 20:25:22 +0800

    同步脚本

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 if [ -n "$1" ]; then
   git reset --hard $1
   echo 😡 当前位于 $1
@@ -6,5 +7,5 @@ else
   git pull
   echo 😄 当前位于最新
 fi
-# yarn release --arch x64
-# cp ./bin/XEngine-linux-x64 /data/f/engine/0.0.45/
+yarn release --arch x64
+cp ./bin/XEngine-linux-x64 /data/f/engine/0.0.45/
