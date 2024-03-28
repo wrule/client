f9927d1f71fb6c68fe28fe9985a45ebc24916178
wrule <1982775886@qq.com>
2024-03-28 19:33:40 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # perfma@888!
-ssh root@10.10.222.240 "source ~/.nvm/nvm.sh && cd /data/wwwroot/client && bash ./sync.sh"
+sshpass -p "perfma@888!" ssh root@10.10.222.240 "source ~/.nvm/nvm.sh && cd /data/wwwroot/client && bash ./sync.sh"
 # zZVhsiWS
-ssh root@10.10.222.154 "cd /data && bash ./xen-update.sh"
+sshpass -p "zZVhsiWS" ssh root@10.10.222.154 "cd /data && bash ./xen-update.sh"
