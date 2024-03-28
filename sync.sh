163975e5544571bed966883bc3be885360cf549d
wrule <1982775886@qq.com>
2024-03-28 20:22:20 +0800

    fix: bug

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ -n "$1" ]; then
   git reset --hard $1
+  echo 😡 当前位于 $1
 else
   git pull
+  echo 😄 当前位于最新
 fi
 yarn release --arch x64
 cp ./bin/XEngine-linux-x64 /data/f/engine/0.0.45/
