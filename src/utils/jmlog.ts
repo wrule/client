d126d4dabf385bde598f2a628a30909be02f77ed
wrule <1982775886@qq.com>
2024-03-29 12:46:56 +0800

    提交代码

---
+++ src/utils/jmlog.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import moment from 'moment';
+
+export default
+function flog(...args: any[]) {
+  const time = moment().format('HH:mm:ss');
+  fs.appendFileSync('~/flog.log', `[${time}]\n`);
+  args.forEach((argv, index) => {
+    fs.appendFileSync('~/flog.log', `\t${index + 1}:\n`);
+    try {
+      fs.appendFileSync('~/flog.log', `\t${JSON.stringify(argv)}\n`);
+    } catch (error) {
+      fs.appendFileSync('~/flog.log', '\tsource: ' + argv);
+    }
+  });
+}
