70d18a8272b47657c49fd532f6601a5b34848ff8
wrule <1982775886@qq.com>
2024-03-29 13:07:51 +0800

    日志

--- src/utils/jmlog.ts
+++ src/utils/jmlog.ts
@@ -4,13 +4,13 @@ import moment from 'moment';
 export default
 function flog(...args: any[]) {
   const time = moment().format('HH:mm:ss');
-  fs.appendFileSync('~/flog.log', `[${time}]\n`);
+  fs.appendFileSync('flog.log', `[${time}]\n`);
   args.forEach((argv, index) => {
-    fs.appendFileSync('~/flog.log', `\t${index + 1}:\n`);
+    fs.appendFileSync('flog.log', `\t${index + 1}:\n`);
     try {
-      fs.appendFileSync('~/flog.log', `\t${JSON.stringify(argv)}\n`);
+      fs.appendFileSync('flog.log', `\t${JSON.stringify(argv)}\n`);
     } catch (error) {
-      fs.appendFileSync('~/flog.log', '\tsource: ' + argv);
+      fs.appendFileSync('flog.log', '\tsource: ' + argv);
     }
   });
 }
