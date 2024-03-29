c3c1f49e840ebee974a67f61ef3c0f4c85ce4f5d
wrule <1982775886@qq.com>
2024-03-29 12:48:00 +0800

    提交

--- src/core/error/assert.ts
+++ src/core/error/assert.ts
@@ -6,6 +6,7 @@ import { AssertResult } from '@/core/types/result/single';
 import { CONTROLLER_ERROR } from '@/core/enum';
 import BaseError from '@/core/error/base';
 import Logger from '@/logger';
+import flog from '@/utils/jmlog';
 // import { ASSERT } from '@/assert';
 
 // const slice = (arg?: string): string => {
@@ -23,8 +24,7 @@ export default class AssertError extends BaseError {
   public constructor(result: AssertResult[]) {
     super();
     const n = result.filter((item) => item.result === false);
-    Logger.info('[JDBC-new]');
-    console.log('[JDBC-new2]');
+    flog('[JDBC-flog]');
     this.message = `${n.length} asserts failed [JDBC]`;
     this.code = CONTROLLER_ERROR.ASSERT_ERROR;
     delete this.stack;
