7ea690777d595b6dc6671f1986e0a36bc80723fc
wrule <1982775886@qq.com>
2024-04-15 17:14:18 +0800

    fix: bug

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 sshpass -p "perfma@888!" ssh root@10.10.222.240 "source ~/.nvm/nvm.sh && cd /data/wwwroot && bash ./sync.sh $1"
 # zZVhsiWS
 # hdgzE35m
-sshpass -p "hdgzE35m" ssh root@10.10.226.33 "cd /data && bash ./xen-update.sh"
+sshpass -p "5d96Pk2R" ssh root@10.10.222.76 "cd /data && bash ./xen-update.sh"
