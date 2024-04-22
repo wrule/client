8f26caa3ceb54519067e98b7146a044dc2d04a51
wrule <1982775886@qq.com>
2024-04-22 10:21:29 +0800

    update set proxy

--- set-proxy.sh
+++ set-proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sshpass -p "5d96Pk2R" ssh root@10.10.222.76 "cd /data/twhale && echo '{\"proxyUrl\": \"http://10.10.222.240:6419\"}' > xconfig.json"
+sshpass -p "5d96Pk2R" ssh root@10.10.222.76 "cd /data/twhale && echo '{\"proxyUrl\": \"http://172.16.0.83:6419\"}' > xconfig.json"
 sshpass -p "5d96Pk2R" ssh root@10.10.222.76 "cd /data && bash ./xen-update.sh"
