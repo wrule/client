ad58b918e799c0a4c2f69fa3f4781aa77099d8b3
wrule <1982775886@qq.com>
2024-05-27 23:34:07 +0800

    noLogger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -5,6 +5,7 @@
 import { configure } from 'log4js';
 import { isDeveloper } from '@/utils';
 import { opts } from '@/config';
+import xconfig from '@/xconfig';
 // [2021-08-23T19:03:03.319] [INFO] src/controller/base.ts:198 ✅ [71] [HTTP] [dev_0_17] status = DONE, time = 8ms
 // [2021-08-23T19:03:03.319] [INFO] ✅ [71] [HTTP] [dev_0_17] status = DONE, time = 8ms
 const pattern = isDeveloper ? '%[[%d] [%p] %f{3}:%l %]%m' : '%[[%d] [%p] %]%m';
@@ -27,7 +28,7 @@ const log4js = configure({
 });
 
 const Logger = log4js.getLogger('XEngine');
-if (isDeveloper) {
+if (xconfig.noLogger) {
   Logger.level = 'OFF';
 }
 const log = console.log;
