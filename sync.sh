63d4e1a9cb57e8482b0b66caf5b5d8ead962e0c2
jimao <shihao.gu@perfma.com>
2024-08-26 11:25:32 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git pull
-if [ -n "$1" ]; then
-  git reset --hard $1
-  echo 😡 当前位于 $1
-else
-  git pull
-  echo 😄 当前位于最新
-fi
-yarn release --arch x64
-cp ./bin/XEngine-linux-x64 /data/f/engine/0.0.50/
+# git pull
+# if [ -n "$1" ]; then
+#   git reset --hard $1
+#   echo 😡 当前位于 $1
+# else
+#   git pull
+#   echo 😄 当前位于最新
+# fi
+yarn release
+# cp ./bin/XEngine-linux-x64 /data/f/engine/0.0.50/
